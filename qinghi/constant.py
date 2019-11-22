6e8d0c65a5fa0ea36f9da7d1a0cff8dadb1f74f5
tree <2332532718@qq.com>
2019-11-22 18:28:33 +0800

    <bugfix>: <切换到正式服务器>
    <body>
    
    <footer>

--- qinghi/constant.py
+++ qinghi/constant.py
@@ -4,5 +4,5 @@ from os import path
 here = path.dirname(__file__)
 parent = path.join(path.split(here)[0])
 
-BASE_URL = 'http://121.42.54.162/'
-# BASE_URL = 'http://www.qinghi.com/'
+# BASE_URL = 'http://121.42.54.162/'
+BASE_URL = 'http://www.qinghi.com/'
